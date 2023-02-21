5e3fd23797fc097b9ef9e41abb420fabd99f9005
Rahul Rauniyar <69071433+Rauniyar09@users.noreply.github.com>
2023-02-21 15:33:56 +0530

    Add files via upload

---
+++ build/precache-manifest.7c71b12bf9b62b85b1df312f4988e647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "573c5063b089a6ac4ae4",
+    "url": "/static/css/main.74fcde6f.chunk.css"
+  },
+  {
+    "revision": "573c5063b089a6ac4ae4",
+    "url": "/static/js/main.573c5063.chunk.js"
+  },
+  {
+    "revision": "6a3571ed405b20e38e78",
+    "url": "/static/js/1.6a3571ed.chunk.js"
+  },
+  {
+    "revision": "c4a13d8c9528d772d945",
+    "url": "/static/js/2.c4a13d8c.chunk.js"
+  },
+  {
+    "revision": "a87f59b1f6516fa8afc0",
+    "url": "/static/js/runtime~main.a87f59b1.js"
+  },
+  {
+    "revision": "e4e9e9722c1393efa10af996560dec90",
+    "url": "/index.html"
+  }
+];
